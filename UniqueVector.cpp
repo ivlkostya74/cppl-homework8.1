83a4bc9828916e2ff133366a815a7639113d5f87
Konstantin Ivlev <ivlkostya@yandex.ru>
2023-08-05 18:28:32 +1000

    first commit

---
+++ UniqueVector.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+	
+	auto print = [](const std::vector<int>& v) {
+		for (const auto& i : v)
+			std::cout << i << ' ';
+		std::cout << std::endl;
+	};
+
+	std::cout << "[IN]: ";
+	print(vec);
+
+	std::sort(vec.begin(), vec.end()); print(vec);
+
+	auto not_unique = std::unique(vec.begin(), vec.end()); print(vec);
+	vec.erase(not_unique, vec.end());
+
+	std::cout << "[OUT]: ";
+	print(vec);
+
+	std::cout << std::endl;
+
+	
+
+	
+
+	
+
+	return 0;
+}
